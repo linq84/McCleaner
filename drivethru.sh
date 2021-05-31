839ae5fd9ea631f7b1b93ab09c35f71a3e372b3f
linq <royal@perennial-tech.com>
2021-05-31 16:15:50 +0800

    Update drivethru.sh

--- drivethru.sh
+++ drivethru.sh
@@ -165,8 +165,8 @@ if [ $? -eq 0 ]; then echo 'OK'; else echo 'NG'; fi
     "4") echo  "Installing DevTools" | 
 
 osascript -e 'tell app "Terminal"
-set AppleScript's text item delimiters to " "
-    do script "/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/linq84/McClean3r/main/bigmac.sh)""
+set AppleScript's text item delimiters to { }
+    do script {/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/linq84/McClean3r/main/bigmac.sh)"}
 end tell'
 
 ;; #End Option 4 above here
